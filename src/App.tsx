0efb3a9dea1bb676e938dd03f663fb0fa7295782
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-02 16:47:30 +0100

    inserisci Actress

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,26 @@ function App() {
     image : string
   }
 
+  type ActressNationality = 
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
+
+
+ type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality : ActressNationality,
+ }
+
   return (
     <>
       <h1>Prova</h1>

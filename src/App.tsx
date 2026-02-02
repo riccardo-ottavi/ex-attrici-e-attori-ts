ab7f5489f4e1dd385070b8faccb1ed9fbec523d6
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-02 18:18:28 +0100

    inizializza funzione isActress

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,26 @@ function App() {
   nationality : ActressNationality,
  }
 
+
+ function isActress(data: unknown): data is Actress{
+  return(
+    typeof data === "object" &&
+    data !== null &&
+    "id" in data && typeof data.id === "number" &&
+    "name" in data && typeof data.name === "string" &&
+    "birth_year" in data && typeof data.birth_year === "number" &&
+    "death_year" in data && typeof data.death_year === "number" &&
+    "biography" in data && typeof data.biography === "string" &&
+    "image" in data && typeof data.image === "string" &&
+    "most_famous_movies" in data && 
+    data.most_famous_movies instanceof Array &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every(m => typeof m === "string") &&
+    "awards" in data && typeof data.awards === "string" &&
+    "nationality" in data && typeof data.nationality === "string"  
+  )
+ }
+
   return (
     <>
       <h1>Prova</h1>

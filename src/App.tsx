bcea72a1ab9112f63a25206764d9865db81a4579
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-02 16:09:42 +0100

    inserisci type Person

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import './App.css'
 
 function App() {
 
+  type Person = {
+    readonly id: number,
+    readonly name : string,
+    birth_year : number,
+    death_year? : number 
+    biography : string,
+    image : string
+  }
+
   return (
     <>
       <h1>Prova</h1>
